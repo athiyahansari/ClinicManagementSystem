2346e8a2bf2f28e851159a004b38e0c31a378fc6
Cheni-10 <esandichenithma@outlook.com>
2025-05-26 11:13:12 +0530

    Login Form for Clinic Management System

--- View/LoginForm.cs
+++ View/LoginForm.cs
@@ -16,5 +16,15 @@ namespace CMS.View
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
