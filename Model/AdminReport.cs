22e141711e8ba37797fc93e48f0be81422a85daf
sumaiya-ri <sumaiyarifkan2@gmail.com>
2025-06-04 19:37:27 +0530

    generate report for admin almost done

---
+++ Model/AdminReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Model
+{
+    internal class AdminReport
+    {
+        public int AppointmentId { get; set; }
+        public string PatientName { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Status { get; set; }
+
+        public string TimeFormatted => Time.ToString(@"hh\:mm");
+
+    }
+}
---
+++ Model/PatientTrend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Model
+{
+    internal class PatientTrend
+    {
+        public string PatientName { get; set; }
+        public int TotalAppointments { get; set; }
+        public DateTime LastAppointmentDate { get; set; }
+    }
+}
