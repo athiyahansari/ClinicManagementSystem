5c6831cf2e618bebf349bab83311f22e35f37483
Cheni-10 <esandichenithma@outlook.com>
2025-06-08 13:06:12 +0530

    Model classes

---
+++ Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Model
+{
+    // Base class Person to apply inheritance
+    public class Person
+    {
+        public int Id { get; set; } // Unique ID
+        public string Name { get; set; } // Name of the person
+    }
+}
