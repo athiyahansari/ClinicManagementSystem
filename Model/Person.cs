726106ced6dcd4a5f4b75922ab41ccd02458e5e1
Cheni-10 <esandichenithma@outlook.com>
2025-06-08 20:58:27 +0530

    model classes

---
+++ Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Model
+{
+    // Base class with shared attributes
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Model
+{
+    // Represents associated user account
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        // Add properties like Password, Role if needed
+    }
+}
